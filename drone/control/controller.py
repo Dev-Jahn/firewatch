feb181eb3926bd4877d9ff874546b3e1e8139f5e
Dev-Jahn <ajh508@naver.com>
2019-06-12 20:05:00 +0900

    'renamed'

---
+++ drone/control/controller.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+# In[34]:
+
+
+import socket
+
+
+# In[35]:
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+# In[36]:
+
+
+sock.sendto("surprise motherfucker".encode(), ('192.168.43.143',8080))
+#sock.sendto("surprise motherfucker".encode(), ('192.168.0.148',8080))
+
+
+# In[37]:
+
+
+sock.sendto("surprise motherfucker".encode(), ('192.168.43.143',6666))
+#sock.sendto("surprise motherfucker".encode(), ('192.168.0.148',8080))
+
--- drone/control/receiver.py
+++ drone/control/receiver.py
@@ -1,22 +1,23 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[3]:
 
 
 import socket
 
 
-# In[2]:
+# In[4]:
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 
-# In[ ]:
+# In[6]:
 
 
-sock.bind(('192.168.43.143',8080))
+sock.bind(('192.168.42.162',8080))
+#sock.bind(('192.168.43.143',8080))
 data,addr = sock.recvfrom(200)
 print('data:',data.decode())
 print('client IP:',addr[0])
